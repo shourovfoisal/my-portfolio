418fcac101444c9d3ec010cb7d33edd6789cb422
Shourov Foisal <shourovfoisal@gmail.com>
2023-08-13 23:03:52 +0600

    added home link to the site logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,8 +7,10 @@ type Props = React.FC<PropsWithChildren & {}>
 export const Navbar: Props = () => {
   return (
     <div className='grid grid-cols-1 md:grid-cols-2'>
-        <div className='text-6xl text-pastelOrange'>
-            <FaCode />
+        <div className='flex text-6xl text-pastelOrange'>
+            <Link href={`/`}>
+              <FaCode />
+            </Link>
         </div>
         <ul className='flex justify-end items-center text-ashGray text-2xl gap-6'>
             <li><Link href={`/services`}>Services</Link></li>
