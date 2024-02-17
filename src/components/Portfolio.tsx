16e34545d03d9634b9b174840767fefa4940ef4c
Shourov Foisal <shourovfoisal@gmail.com>
2024-02-17 15:52:03 +0600

    Improved the responsiveness of the image card

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -6,18 +6,25 @@ import { PropsWithChildren } from "react";
 type Props = React.FC<PropsWithChildren & {}>;
 
 export const Portfolio: Props = () => {
+  const mockData = [
+    {
+      title: "Movies Zone",
+      categories: ["Movies", "Audiences"],
+    },
+  ];
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-10 lg:gap-20">
       <div className="col-span-1">
         <div className="flex flex-col gap-10 lg:gap-20">
           <AppCardTransparent {...transparentCardData.portfolioSection} />
-          <AppCardImage image={website_1} />
+          <AppCardImage image={website_1} {...mockData[0]} />
         </div>
       </div>
       <div className="col-span-1">
         <div className="ml-auto flex flex-col gap-10 lg:gap-20">
-          <AppCardImage image={website_1} />
-          <AppCardImage image={website_1} />
+          <AppCardImage image={website_1} {...mockData[0]} />
+          <AppCardImage image={website_1} {...mockData[0]} />
         </div>
       </div>
     </div>
--- src/components/common/AppCardImage.tsx
+++ src/components/common/AppCardImage.tsx
@@ -1,16 +1,28 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import { RFC } from "./Types";
 
-export type AppCardImageProps = { image: any };
+export type AppCardImageProps = {
+  image: string | StaticImageData;
+  title: string;
+  categories: string[];
+};
 
-export const AppCardImage: RFC<AppCardImageProps> = ({ image }) => {
+export const AppCardImage: RFC<AppCardImageProps> = ({
+  image,
+  categories,
+  title,
+}) => {
   return (
-    <div className="w-full bg-NightBlack flex flex-col gap-16 md:gap-6 xl:gap-16 aspect-[1/1.1] overflow-hidden cursor-pointer">
+    <div className="w-full bg-NightBlack flex flex-col gap-4 md:gap-6 xl:gap-16 aspect-[1/1.1] overflow-hidden cursor-pointer">
       <div className="flex justify-between items-center text-LightPastelOrange p-10 pb-0 md:p-5 md:pb-0 lg:p-10 lg:pb-0">
-        <h2 className="text-3xl md:text-2xl xl:text-3xl">Movies Zone</h2>
-        <h3 className="text-sm md:text-xs xl:text-sm flex gap-2">
-          <span>Movies,</span>
-          <span>Audiences</span>
+        <h2 className="text-xl xs:text-2xl sm:text-3xl md:text-2xl xl:text-3xl">
+          {title}
+        </h2>
+        <h3 className="text-sm md:text-xs lg:text-sm flex gap-2">
+          {Array.isArray(categories) &&
+            categories.map((category) => (
+              <span key={category}>{category}</span>
+            ))}
         </h3>
       </div>
       <div>
